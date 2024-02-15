1d10a01e90dee4a316461d6eff8f762369259a73
Alp <alpefeka2001@gmail.com>
2024-02-15 18:18:06 -0500

    change signs config

--- convert_data.py
+++ convert_data.py
@@ -3,10 +3,24 @@ import csv
 import yaml
 from PIL import Image
 
+UPPER_LIMIT = 5
+
+annotation_source_path = "/home/alp/AVTPerception/February/AVT-Perception/datasets/signs/allAnnotations.csv"
+train_path = "/home/alp/AVTPerception/February/AVT-Perception/datasets/signs/labels/train"
+
+
 # open the yaml file
 with open("signs-config.yaml", "r") as file:
     config = yaml.safe_load(file)
     names = config["names"]
+    path = "datasets/signs"
+    images_train_path = path + "/" + config.get("train")
+    val_train_path = path + "/" + config.get("val")
+
+
+def getTrueLocationFromCSV(file_name_in_csv : str):
+    return "/home/alp/AVTPerception/February/AVT-Perception/datasets/signs/images/train" + "/" + file_name_in_csv
+
 
 def getIndexFromName(class_name : str):
     return list(names.keys())[list(names.values()).index(class_name)]
@@ -22,19 +36,19 @@ def convertToYoloAnnotation(file_name : str, class_name : str, x_uleft : int, y_
     img_width, img_height = img.size
 
     # finding normalized width
-    width = x_uleft - x_lright
+    width = abs(x_uleft - x_lright)
     normalized_width = width / img_width
 
     # finding normalized height
-    height = y_uleft - y_lright
+    height = abs(y_uleft - y_lright)
     normalized_height = height / img_height
      
     # finding x center:
-    x_center_pxl = (x_uleft + x_lright) / 2
+    x_center_pxl = (x_uleft + x_lright) / 2.0
     x_center = x_center_pxl / img_width
 
     # finding y center
-    y_center_pxl = (y_uleft + y_lright) / 2
+    y_center_pxl = (y_uleft + y_lright) / 2.0
     y_center = y_center_pxl / img_height
 
     # getting class id
@@ -43,12 +57,44 @@ def convertToYoloAnnotation(file_name : str, class_name : str, x_uleft : int, y_
     
     return class_id, x_center, y_center, normalized_width, normalized_height
 
+def createAnnotations():
+    #os.chdir("/home/alp/AVTPerception/February/AVT-Perception")
+    counter = 0
+    with open(annotation_source_path) as csvFile:
+        reader = csv.DictReader(csvFile, delimiter = ";")
+        for row in reader:
+            YOLO_formatted = convertToYoloAnnotation( getTrueLocationFromCSV(row["Filename"]), row["Annotation tag"],
+                                   int(row["Upper left corner X"]), int(row["Upper left corner Y"]), 
+                                   int(row["Lower right corner X"]), int(row["Lower right corner Y"]))
+            createYoloAnnotation(row["Filename"] + ".txt", YOLO_formatted)
+            if counter >= UPPER_LIMIT:
+                return
+            counter += 1
+    
 
+def createYoloAnnotation( file_name: str ,YOLO_formatted: tuple):
+    os.chdir(train_path)
+    with open(file_name, "a") as new_file:
+        line = " ".join(YOLO_formatted) + "\n"
+        new_file.write(line)
 
 
 # TESTING
-test = ["datasets/signs/aiua120214-0/frameAnnotations-DataLog02142012_external_camera.avi_annotations/speedLimit_1330545914.avi_image0.png",
-         "speedLimit25", 	529,	183,	546, 203]
 
-test_out = convertToYoloAnnotation(test[0], test[1], test[2], test[3], test[4], test[5])
-print(test_out)
+# Testing for convertion logic
+
+# with open("./datasets/signs/allAnnotations.csv") as csvFile:
+#     reader = csv.DictReader(csvFile, delimiter = ";")os.chdir("/home/alp/AVTPerception/February/AVT-Perception/datasets/signs/labels/train")
+#     limiter = 0
+#     for file_test in reader:
+#         out = convertToYoloAnnotation( getTrueLocationFromCSV(file_test["Filename"]), file_test["Annotation tag"],
+#                                    int(file_test["Upper left corner X"]), int(file_test["Upper left corner Y"]), 
+#                                    int(file_test["Lower right corner X"]), int(file_test["Lower right corner Y"]))
+#         print(out[0], out[1], out[2], out[3], out[4])
+#         limiter += 1
+#         # test for 5 values
+#         if limiter == 5:
+#             break
+
+
+createAnnotations()
