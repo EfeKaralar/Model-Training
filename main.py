acf5674c23810f53f1635e296bbe9907ebc2ea83
Alp <alpefeka2001@gmail.com>
2024-02-26 17:16:28 -0500

    export functionality

--- main.py
+++ main.py
@@ -3,5 +3,11 @@ from ultralytics import YOLO
 # load a model
 model = YOLO("yolov8n.yaml")    # build a new model from scratch
 
-# use the model
-results = model.train(data="signs-config.yaml", epochs=1)  #train the model
+# train the model for 300 epochs
+results = model.train(data="signs-config.yaml", epochs=300)  #train the model
+
+# Evaluate the model's performance on the validation set
+results = model.val()
+
+# Export the model to ONNX format
+success = model.export(format='onnx')
